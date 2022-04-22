4994620470b0280df23aadd51a7ea6f9a13938ea
Dorian Desblancs <dorian.desblancs@mail.mcgill.ca>
2022-04-22 17:07:41 +0000

    Refacto cross data.

--- cross_data.py
+++ cross_data.py
@@ -4,6 +4,8 @@ import random
 import pickle
 import numpy as np
 
+from loader import load_models
+from epochs import train_epoch, val_epoch
 from models.models import DS_CNN, Down_CNN
 from processing.evaluate import beat_tracking as eval
 
@@ -21,15 +23,15 @@ def train_model(
 ):
     """
     Function for training model on Hainsworth data set.
-    -- train_wavs       : list of training wav files (dictionary keys)
-    -- train_vqts       : training spectrograms of audio
-    -- train_masks      : training beat tracking pulse vectors
+    -- train_wavs : list of training wav files (dictionary keys)
+    -- train_vqts : training spectrograms of audio
+    -- train_masks : training beat tracking pulse vectors
     -- train_real_times : training list with real beat tracking times in seconds
-    -- test_wavs        : list of test wav files (dictionary keys)
-    -- test_vqts        : test spectrograms of audio
-    -- test_masks       : test tracking pulse vectors
-    -- test_real_times  : test list with real beat tracking times in seconds
-    -- ymldict          : YAML parameters
+    -- test_wavs : list of test wav files (dictionary keys)
+    -- test_vqts : test spectrograms of audio
+    -- test_masks : test tracking pulse vectors
+    -- test_real_times : test list with real beat tracking times in seconds
+    -- ymldict : YAML parameters
     """
     # Load the Hainsworth stuff:
     _status = ymldict.get("cross_status")
@@ -67,43 +69,9 @@ def train_model(
     ig = []
 
     for jj in range(8):
-
-        # Set loss function
-        criterion = torch.nn.BCELoss().cuda()
-
-        # Set model and pre-trained layers if need be; optimizer set accordingly
-        if _status == "pretrained":
-            print("Pretrained learning mode...")
-
-            model = Down_CNN().cuda()
-            state_dict = torch.load(
-                "models/saved/shift_pret_cnn_16.pth", map_location=torch.device("cuda")
-            )
-            model.pretext.load_state_dict(state_dict)
-
-            if _pre == "frozen":
-                for param in model.pretext.anchor.pretrained.parameters():
-                    param.requires_grad = False
-
-                for param in model.pretext.postve.pretrained.parameters():
-                    param.requires_grad = False
-
-                optimizer = torch.optim.Adam(
-                    filter(lambda p: p.requires_grad, model.parameters()),
-                    lr=_lr,
-                    betas=(0.9, 0.999),
-                )
-
-            elif _pre == "finetune":
-                optimizer = torch.optim.Adam(
-                    model.parameters(), lr=0.5 * _lr * 10e-2, betas=(0.9, 0.999)
-                )
-
-        else:
-            print("Vanilla learning mode...")
-            model = DS_CNN(pretext=True).cuda()
-            optimizer = torch.optim.Adam(model.parameters(), lr=_lr, betas=(0.9, 0.999))
-
+        # Load everything
+        criterion, optimizer, model = load_models(_status, _pre, _lr)
+        
         val_counter = 0
 
         train_loss = []
@@ -113,9 +81,7 @@ def train_model(
         train_indices = []
 
         for ii in range(8):
-            if ii == jj:
-                continue
-            else:
+            if ii != jj:
                 train_indices = train_indices + splits[ii]
 
         val_indices = splits[jj]
@@ -125,162 +91,53 @@ def train_model(
         best_f1 = 0.0
 
         # Train model
-        for epoch in range(500):
+        for epoch in range(1):
             print("\n-- Epoch {} --".format(epoch))
 
-            full_train_loss = 0.0
-            full_val_loss = 0.0
             (
+                model,
+                optimizer,
+                full_train_loss,
                 train_f_measure,
-                train_cmlc,
-                train_cmlt,
-                train_amlc,
-                train_amlt,
-                train_info_gain,
-            ) = (0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-            val_f_measure, val_cmlc, val_cmlt, val_amlc, val_amlt, val_info_gain = (
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-                0.0,
+                _,
+                _,
+                _,
+                _,
+                _,
+            ) = train_epoch(
+                model,
+                criterion,
+                optimizer,
+                _status,
+                train_indices,
+                train_real_times,
+                train_vqts,
+                train_masks,
+                threshold,
             )
-
-            model.train()
-
-            for batch_idx, wav in enumerate(train_indices):
-                times = train_real_times[wav]
-
-                if _status == "pretrained":
-                    vqt = train_vqts[wav]
-                    vqt1 = torch.reshape(
-                        vqt[0, :, :], (1, 1, vqt.shape[1], vqt.shape[2])
-                    ).cuda()
-                    vqt2 = torch.reshape(
-                        vqt[1, :, :], (1, 1, vqt.shape[1], vqt.shape[2])
-                    ).cuda()
-
-                    msk = train_masks[wav]
-                    msk = torch.reshape(msk, (1, msk.shape[0])).cuda()
-
-                    optimizer.zero_grad()
-
-                    # Apply model to full batch
-                    output = model(vqt1, vqt2)
-
-                    loss = criterion(output, msk)
-                    loss.backward()
-                    optimizer.step()
-
-                else:
-                    vqt = train_vqts[wav]
-                    vqt = torch.reshape(
-                        vqt[:, :], (1, 1, vqt.shape[0], vqt.shape[1])
-                    ).cuda()
-
-                    msk = train_masks[wav]
-                    msk = torch.reshape(msk, (1, msk.shape[0])).cuda()
-
-                    optimizer.zero_grad()
-
-                    # Apply model to full batch
-                    output = model(vqt)
-
-                    loss = criterion(output, msk)
-                    loss.backward()
-                    optimizer.step()
-
-                full_train_loss += loss.item()
-
-                cpu_output = output.squeeze(0).cpu().detach().numpy()
-
-                res = eval(cpu_output, times, threshold=threshold)
-                train_f_measure += res[0]
-                train_cmlc += res[1]
-                train_cmlt += res[2]
-                train_amlc += res[3]
-                train_amlt += res[4]
-                train_info_gain += res[5]
-
-            full_train_loss = full_train_loss / (batch_idx + 1)
-            train_f_measure = train_f_measure / (batch_idx + 1)
-            train_cmlc = train_cmlc / (batch_idx + 1)
-            train_cmlt = train_cmlt / (batch_idx + 1)
-            train_amlc = train_amlc / (batch_idx + 1)
-            train_amlt = train_amlt / (batch_idx + 1)
-            train_info_gain = train_info_gain / (batch_idx + 1)
 
             print("\nMean training loss is {:.3f}.".format(full_train_loss))
             print("Mean F1-score is {:.3f}.".format(train_f_measure))
 
-            model.eval()
-
-            for batch_idx, wav in enumerate(val_indices):
-                with torch.no_grad():
-                    times = train_real_times[wav]
-
-                    if _status == "pretrained":
-                        vqt = train_vqts[wav]
-                        vqt1 = torch.reshape(
-                            vqt[0, :, :], (1, 1, vqt.shape[1], vqt.shape[2])
-                        ).cuda()
-                        vqt2 = torch.reshape(
-                            vqt[1, :, :], (1, 1, vqt.shape[1], vqt.shape[2])
-                        ).cuda()
-
-                        msk = train_masks[wav]
-                        msk = torch.reshape(msk, (1, msk.shape[0])).cuda()
-
-                        output = model(vqt1, vqt2)
-
-                        loss = criterion(output, msk)
-
-                    else:
-                        vqt = train_vqts[wav]
-                        vqt = torch.reshape(
-                            vqt[:, :], (1, 1, vqt.shape[0], vqt.shape[1])
-                        ).cuda()
-
-                        msk = train_masks[wav]
-                        msk = torch.reshape(msk, (1, msk.shape[0])).cuda()
-
-                        # Apply model to full batch
-                        output = model(vqt)
-
-                        loss = criterion(output, msk)
-
-                    full_val_loss += loss.item()
-
-                    cpu_output = output.squeeze(0).cpu().detach().numpy()
-
-                    res = eval(cpu_output, times, threshold=threshold)
-                    val_f_measure += res[0]
-                    val_cmlc += res[1]
-                    val_cmlt += res[2]
-                    val_amlc += res[3]
-                    val_amlt += res[4]
-                    val_info_gain += res[5]
-
-            full_val_loss = full_val_loss / (batch_idx + 1)
-            val_f_measure = val_f_measure / (batch_idx + 1)
-            val_cmlc = val_cmlc / (batch_idx + 1)
-            val_cmlt = val_cmlt / (batch_idx + 1)
-            val_amlc = val_amlc / (batch_idx + 1)
-            val_amlt = val_amlt / (batch_idx + 1)
-            val_info_gain = val_info_gain / (batch_idx + 1)
+            full_val_loss, val_f_measure, _, _, _, _, _ = val_epoch(
+                model,
+                criterion,
+                _status,
+                val_indices,
+                train_real_times,
+                train_vqts,
+                train_masks,
+                threshold,
+            )
 
             print("\nMean validation loss     is {:.3f}.".format(full_val_loss))
             print("Mean validation F1-score is {:.3f}.".format(val_f_measure))
 
             if val_f_measure > best_f1:
                 mod_fp = "models/saved/cross_{}_{}.pth".format(_train_set, _status)
-
                 best_f1 = val_f_measure
-
                 torch.save(model.state_dict(), mod_fp)
                 print("Saved model to " + mod_fp)
-
                 val_counter = 0
 
             else:
@@ -299,79 +156,31 @@ def train_model(
         if _status == "pretrained":
             test_mod = Down_CNN().cuda()
         else:
-            test_mod = DS_CNN(pretext=True).cuda()
+            test_mod = DS_CNN().cuda()
 
         state_dict = torch.load(mod_fp)
         test_mod.load_state_dict(state_dict)
 
-        test_mod.eval()
-
-        full_test_loss = 0.0
-        test_f_measure, test_cmlc, test_cmlt, test_amlc, test_amlt, test_info_gain = (
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
+        (
+            full_test_loss,
+            test_f_measure,
+            test_cmlc,
+            test_cmlt,
+            test_amlc,
+            test_amlt,
+            test_info_gain,
+        ) = val_epoch(
+            test_mod,
+            criterion,
+            _status,
+            test_wavs,
+            test_real_times,
+            test_vqts,
+            test_masks,
+            threshold,
         )
-
-        for batch_idx, wav in enumerate(test_wavs):
-            with torch.no_grad():
-                times = test_real_times[wav]
-
-                if _status == "pretrained":
-                    vqt = test_vqts[wav]
-                    vqt1 = torch.reshape(
-                        vqt[0, :, :], (1, 1, vqt.shape[1], vqt.shape[2])
-                    ).cuda()
-                    vqt2 = torch.reshape(
-                        vqt[1, :, :], (1, 1, vqt.shape[1], vqt.shape[2])
-                    ).cuda()
-
-                    msk = test_masks[wav]
-                    msk = torch.reshape(msk, (1, msk.shape[0])).cuda()
-
-                    output = model(vqt1, vqt2)
-
-                    loss = criterion(output, msk)
-
-                else:
-                    vqt = test_vqts[wav]
-                    vqt = torch.reshape(
-                        vqt[:, :], (1, 1, vqt.shape[0], vqt.shape[1])
-                    ).cuda()
-
-                    msk = test_masks[wav]
-                    msk = torch.reshape(msk, (1, msk.shape[0])).cuda()
-
-                    # Apply model to full batch
-                    output = model(vqt)
-
-                    loss = criterion(output, msk)
-
-                full_test_loss += loss.item()
-
-                cpu_output = output.squeeze(0).cpu().detach().numpy()
-
-                res = eval(cpu_output, times, threshold=threshold)
-                test_f_measure += res[0]
-                test_cmlc += res[1]
-                test_cmlt += res[2]
-                test_amlc += res[3]
-                test_amlt += res[4]
-                test_info_gain += res[5]
-
-        full_test_loss = full_test_loss / (batch_idx + 1)
-        test_f_measure = test_f_measure / (batch_idx + 1)
-        test_cmlc = test_cmlc / (batch_idx + 1)
-        test_cmlt = test_cmlt / (batch_idx + 1)
-        test_amlc = test_amlc / (batch_idx + 1)
-        test_amlt = test_amlt / (batch_idx + 1)
-        test_info_gain = test_info_gain / (batch_idx + 1)
-
+        
         print("\n-- Test Set --")
-
         print("\nMean test loss     is {:.3f}.".format(full_test_loss))
         print("Mean beat F1-score is {:.3f}.".format(test_f_measure))
         print("Mean beat CMLC     is {:.3f}.".format(test_cmlc))
@@ -395,7 +204,6 @@ def train_model(
     ig = np.asarray(ig)
 
     print("\n8-fold CV results:")
-
     print("\nBeat F1-score is {:.3f} +- {:.3f}.".format(np.mean(f1), np.std(f1)))
     print("Beat CMLC     is {:.3f} +- {:.3f}.".format(np.mean(cmlc), np.std(cmlc)))
     print("Beat CMLT     is {:.3f} +- {:.3f}.".format(np.mean(cmlt), np.std(cmlt)))
@@ -416,79 +224,79 @@ if __name__ == "__main__":
 
     print("Loading audio and pulses...")
 
-    with open("GTZAN/wavs.pkl", "rb") as handle:
+    with open("data/GTZAN/wavs.pkl", "rb") as handle:
         test_wavs = pickle.load(handle)
 
-    with open("GTZAN/beat_pulses.pkl", "rb") as handle:
+    with open("data/GTZAN/beat_pulses.pkl", "rb") as handle:
         test_masks = pickle.load(handle)
 
-    with open("GTZAN/real_beat_times.pkl", "rb") as handle:
+    with open("data/GTZAN/real_beat_times.pkl", "rb") as handle:
         test_real_times = pickle.load(handle)
 
     if _status == "pretrained":
-        with open("GTZAN/vqts_spleeted.pkl", "rb") as handle:
+        with open("data/GTZAN/vqts_spleeted.pkl", "rb") as handle:
             test_vqts = pickle.load(handle)
 
     else:
-        with open("GTZAN/vqts_original.pkl", "rb") as handle:
+        with open("data/GTZAN/vqts_original.pkl", "rb") as handle:
             test_vqts = pickle.load(handle)
 
     if _train_set == "hainsworth":
-        with open("Hainsworth/wavs.pkl", "rb") as handle:
+        with open("data/Hainsworth/wavs.pkl", "rb") as handle:
             train_wavs = pickle.load(handle)
 
-        with open("Hainsworth/beat_pulses.pkl", "rb") as handle:
+        with open("data/Hainsworth/beat_pulses.pkl", "rb") as handle:
             train_masks = pickle.load(handle)
 
-        with open("Hainsworth/real_beat_times.pkl", "rb") as handle:
+        with open("data/Hainsworth/real_beat_times.pkl", "rb") as handle:
             train_real_times = pickle.load(handle)
 
         if _status == "pretrained":
-            with open("Hainsworth/vqts_spleeted.pkl", "rb") as handle:
+            with open("data/Hainsworth/vqts_spleeted.pkl", "rb") as handle:
                 train_vqts = pickle.load(handle)
 
         else:
-            with open("Hainsworth/vqts_original.pkl", "rb") as handle:
+            with open("data/Hainsworth/vqts_original.pkl", "rb") as handle:
                 train_vqts = pickle.load(handle)
 
     elif _train_set == "smc":
-        with open("SMC/smc_wavs.pkl", "rb") as handle:
+        with open("data/SMC/wavs.pkl", "rb") as handle:
             train_wavs = pickle.load(handle)
 
-        with open("SMC/smc_pulses.pkl", "rb") as handle:
+        with open("data/SMC/pulses.pkl", "rb") as handle:
             train_masks = pickle.load(handle)
 
-        with open("SMC/smc_real_times.pkl", "rb") as handle:
+        with open("data/SMC/real_times.pkl", "rb") as handle:
             train_real_times = pickle.load(handle)
 
         if _status == "pretrained":
-            with open("SMC/smc_vqts_spleeted.pkl", "rb") as handle:
+            with open("data/SMC/vqts_spleeted.pkl", "rb") as handle:
                 train_vqts = pickle.load(handle)
 
         else:
-            with open("SMC/smc_vqts_original.pkl", "rb") as handle:
+            with open("data/SMC/vqts_original.pkl", "rb") as handle:
                 train_vqts = pickle.load(handle)
 
     elif _train_set == "ballroom":
-        with open("Ballroom/wavs.pkl", "rb") as handle:
+        with open("data/Ballroom/wavs.pkl", "rb") as handle:
             train_wavs = pickle.load(handle)
 
-        with open("Ballroom/beat_pulses.pkl", "rb") as handle:
+        with open("data/Ballroom/beat_pulses.pkl", "rb") as handle:
             train_masks = pickle.load(handle)
 
-        with open("Ballroom/real_beat_times.pkl", "rb") as handle:
+        with open("data/Ballroom/real_beat_times.pkl", "rb") as handle:
             train_real_times = pickle.load(handle)
 
         if _status == "pretrained":
-            with open("Ballroom/vqts_spleeted.pkl", "rb") as handle:
+            with open("data/Ballroom/vqts_spleeted.pkl", "rb") as handle:
                 train_vqts = pickle.load(handle)
 
         else:
-            with open("Ballroom/vqts_original.pkl", "rb") as handle:
+            with open("data/Ballroom/vqts_original.pkl", "rb") as handle:
                 train_vqts = pickle.load(handle)
 
     else:
-        print("YAML file for cross data set has a bug in experiment definition!")
+        print("\nYAML file for cross data set has a bug in experiment definition!")
 
     _ = train_model(
         train_wavs,
