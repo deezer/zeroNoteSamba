f9dfec7da203e629a109adf133dfd412e6f83712
Dorian Desblancs <dorian.desblancs@mail.mcgill.ca>
2022-04-22 09:06:27 +0000

    Edited number of epochs.

--- beat_down.py
+++ beat_down.py
@@ -90,7 +90,7 @@ def train_model(wavs, vqts, masks, real_times, data_set, ymldict):
             best_f1 = 0.0
 
             # Train model
-            for epoch in range(1):
+            for epoch in range(500):
                 print("\n-- Epoch {} --".format(epoch))
 
                 (
