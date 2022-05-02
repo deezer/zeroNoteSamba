d216362583986c807fc2961ddbb3e648690abebe
Dorian Desblancs <dorian.desblancs@mail.mcgill.ca>
2022-05-02 10:40:18 +0000

    Changed model loading.

--- loader.py
+++ loader.py
@@ -6,21 +6,23 @@ from models.models import DS_CNN, Down_CNN
 def load_models(_status, _pre, _lr):
     """
     Function for loading loss, optimizer, and model.
-    -- _status : pretrained or vanilla?
+    -- _status : pretrained, vanilla, clmr, or samplecnn?
     -- _pre    : frozen weights
     -- _lr     : learning rate
     """
     # Set loss function
     criterion = torch.nn.BCELoss().cuda()
 
+    print("\n{} learning mode...".format(_status))
+
     # Set model and pre-trained layers if need be; optimizer set accordingly
     if _status == "pretrained":
-        print("\nPretrained learning mode...")
-
         model = Down_CNN().cuda()
+
         state_dict = torch.load(
             "models/saved/shift_pret_cnn_16.pth", map_location=torch.device("cuda")
         )
+
         model.pretext.load_state_dict(state_dict)
 
         if _pre == "frozen":
@@ -36,13 +38,36 @@ def load_models(_status, _pre, _lr):
                 betas=(0.9, 0.999),
             )
 
-        elif _pre == "finetune":
+        else:
+            optimizer = torch.optim.Adam(
+                model.parameters(), lr=0.5 * _lr * 10e-2, betas=(0.9, 0.999)
+            )
+
+    elif _status == "clmr":
+        model = DS_CNN().cuda()
+
+        state_dict = torch.load(
+            "models/saved/clmr_pret_cnn_16.pth", map_location=torch.device("cuda")
+        )
+
+        model.load_state_dict(state_dict)
+
+        if _pre == "frozen":
+            for param in model.pretrained.parameters():
+                param.requires_grad = False
+
+            optimizer = torch.optim.Adam(
+                filter(lambda p: p.requires_grad, model.parameters()),
+                lr=_lr,
+                betas=(0.9, 0.999),
+            )
+
+        else:
             optimizer = torch.optim.Adam(
                 model.parameters(), lr=0.5 * _lr * 10e-2, betas=(0.9, 0.999)
             )
 
     else:
-        print("\nVanilla learning mode...")
         model = DS_CNN().cuda()
         optimizer = torch.optim.Adam(model.parameters(), lr=_lr, betas=(0.9, 0.999))
 
